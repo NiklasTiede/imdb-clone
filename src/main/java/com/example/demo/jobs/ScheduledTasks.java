296e97718eb93b7b83ef9c5184468cece5249272
Niklas Tiede <niklastiede2@gmail.com>
2022-06-23 23:50:24 +0200

    reduce job execution rate

--- src/main/java/com/example/demo/jobs/ScheduledTasks.java
+++ src/main/java/com/example/demo/jobs/ScheduledTasks.java
@@ -14,7 +14,7 @@ public class ScheduledTasks {
 
   private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-  @Scheduled(fixedRate = 5000)
+  @Scheduled(fixedRate = 30000)
   public void reportCurrentTime() {
 
     log.info("The time is now {}", dateFormat.format(new Date()));
