c940d1499b9b610a3609fae99af7a8e3d93e80dc
Niklas Tiede <niklastiede2@gmail.com>
2022-10-09 16:36:28 +0200

    convert birthday dtype from timestamp to date, reorder create table statements and improve test-data

--- src/main/resources/sql-data/schema.sql
+++ src/main/resources/sql-data/schema.sql
@@ -18,25 +18,6 @@ create table movie (
     primary key (id)
 );
 
--- analyze which length is good optimal for the prefixed index:
-SELECT
-    ROUND(SUM(LENGTH(m.primary_title)<10)*100/COUNT(m.primary_title),2) AS pct_length_10,
-    ROUND(SUM(LENGTH(m.primary_title)<20)*100/COUNT(m.primary_title),2) AS pct_length_20,
-    ROUND(SUM(LENGTH(m.primary_title)<50)*100/COUNT(m.primary_title),2) AS pct_length_50,
-    ROUND(SUM(LENGTH(m.primary_title)<100)*100/COUNT(m.primary_title),2) AS pct_length_100
-FROM IMDBCLONE.movie m;
-
--- create prefixed index to speed up LIKE queries starting from beginning of field (example: 'term%')
-create index movie_idx on IMDBCLONE.movie (primary_title(100));
-alter table IMDBCLONE.movie drop index movie_idx;
-
--- fulltext search (I have to see its potential yet)
-create fulltext index movie_title_ft_idx on IMDBCLONE.movie(primary_title(100));
-alter table IMDBCLONE.movie drop index movie_title_ft_idx;
-
--- example query:
-SELECT * FROM IMDBCLONE.movie m WHERE MATCH(m.primary_title) AGAINST('Conjuring' IN NATURAL LANGUAGE MODE) ORDER BY m.imdb_rating_count DESC;
-
 create table account (
     id bigint AUTO_INCREMENT,
     username varchar(255) unique not null,
@@ -46,12 +27,27 @@ create table account (
     last_name varchar(255),
     bio text,
     phone varchar(20),
-    birthday timestamp,
+    birthday date,
     created_at_in_utc timestamp default (utc_timestamp),
     modified_at_in_utc timestamp,
     locked boolean not null,
     enabled boolean not null,
     primary key (id)
+);
+
+create table role (
+    id bigint AUTO_INCREMENT,
+    name varchar(255) unique not null,
+    created_at_in_utc timestamp default (utc_timestamp),
+    primary key (id)
+);
+
+create table account_roles (
+    account_id bigint not null,
+    roles_id bigint not null,
+    created_at_in_utc timestamp default (utc_timestamp),
+    modified_at_in_utc timestamp default (utc_timestamp),
+    primary key (account_id, roles_id)
 );
 
  create table rating (
@@ -90,27 +86,24 @@ create table verification_token (
     primary key (id)
 );
 
-create table account_role (
-    id bigint AUTO_INCREMENT,
-    account_id bigint not null,
-    verification_type varchar(100) not null,
-    token varchar(36) not null,
-    expiry_date_in_utc timestamp not null,
-    confirmed_at_in_utc timestamp,
-    primary key (id)
-);
+--  Add index after movie data import!
 
-create table role (
-    id bigint AUTO_INCREMENT,
-    name varchar(255) unique not null,
-    created_at_in_utc timestamp default (utc_timestamp),
-    primary key (id)
-);
+-- analyze which length is good optimal for the prefixed index:
+SELECT
+    ROUND(SUM(LENGTH(m.primary_title)<10)*100/COUNT(m.primary_title),2) AS pct_length_10,
+    ROUND(SUM(LENGTH(m.primary_title)<20)*100/COUNT(m.primary_title),2) AS pct_length_20,
+    ROUND(SUM(LENGTH(m.primary_title)<50)*100/COUNT(m.primary_title),2) AS pct_length_50,
+    ROUND(SUM(LENGTH(m.primary_title)<100)*100/COUNT(m.primary_title),2) AS pct_length_100
+FROM IMDBCLONE.movie m;
 
-create table account_roles (
-    account_id bigint not null,
-    roles_id bigint not null,
-    created_at_in_utc timestamp default (utc_timestamp),
-    modified_at_in_utc timestamp default (utc_timestamp),
-    primary key (account_id, roles_id)
-);
+-- create prefixed index to speed up LIKE queries starting from beginning of field (example: 'term%')
+create index movie_idx on IMDBCLONE.movie (primary_title(100));
+alter table IMDBCLONE.movie drop index movie_idx;
+
+-- fulltext search (I have to see its potential yet)
+create fulltext index movie_title_ft_idx on IMDBCLONE.movie(primary_title(100));
+alter table IMDBCLONE.movie drop index movie_title_ft_idx;
+
+-- example query:
+SELECT * FROM IMDBCLONE.movie m WHERE MATCH(m.primary_title) AGAINST('Conjuring' IN NATURAL LANGUAGE MODE) ORDER BY m.imdb_rating_count DESC;
+
--- src/main/resources/sql-data/test-data.sql
+++ src/main/resources/sql-data/test-data.sql
@@ -1,29 +1,23 @@
 
-# test data
-insert into IMDBCLONE.movie(primary_title,original_title,start_year,end_year,RUNTIME_MINUTES,modified_at_in_utc,created_at_in_utc,MOVIE_GENRE,MOVIE_TYPE,IMDB_RATING,IMDB_RATING_COUNT,ADULT) values ('Le clown et ses chiens','Le clown et ses chiens',1988, 1989, 120, null, null, 2433, 1,5.8,4299, 0);
-
-insert into IMDBCLONE.account(username,email,password,first_name,last_name,bio,phone,birthday,locked,enabled) values('berndasbrot','niklas@gmail.com','28dh36GHD23gh!!dj','niklas','tiede','ich bin student.','015122973088', '1993-03-15 00:00:00',false,false);
-insert into IMDBCLONE.account(username,email,password,first_name,last_name,bio,phone,birthday,locked,enabled) values('hotgirl18','mel@yahoo.de','12345678','Eva','Adam','hey guys.','01714927653', '1999-02-19 00:00:00',false,false);
-insert into IMDBCLONE.account(username,email,password,first_name,last_name,bio,phone,birthday,locked,enabled) values('Bongoman','schnuggi@yahoo.de','1345338','Bryan','Cranston','hey guys.','01628264723', '1984-08-25 00:00:00',false,false);
-
-insert into IMDBCLONE.rating(rating,movie_id,account_id) values(5.7,1457767,1);
-insert into IMDBCLONE.rating(rating,movie_id,account_id) values(6.7,1457767,2);
-insert into IMDBCLONE.rating(rating,movie_id,account_id) values(7.1,10133422,1);
-insert into IMDBCLONE.rating(rating,movie_id,account_id) values(6.7,10133422,2);
-
-insert into IMDBCLONE.watched_movie(movie_id,account_id) values(1457767,1);
-insert into IMDBCLONE.watched_movie(movie_id,account_id) values(1396484,1);
-insert into IMDBCLONE.watched_movie(movie_id,account_id) values(1457767,2);
-
-insert into IMDBCLONE.comment(message,movie_id,account_id) values('Message1',1457767,1);
-insert into IMDBCLONE.comment(message,movie_id,account_id) values('Message2',10133422,2);
-insert into IMDBCLONE.comment(message,movie_id,account_id) values('Message3',1457767,1);
-insert into IMDBCLONE.comment(message,movie_id,account_id) values('Message4',10133422,1);
-
-insert into IMDBCLONE.verification_token  (verification_type,token,expiry_date_in_utc,account_id) values('EMAIL_CONFIRMATION','ebde1c2c-5960-421a-9e71-2c1161f3e9e4', now(), 1);
-insert into IMDBCLONE.verification_token  (verification_type,token,expiry_date_in_utc,account_id) values('PASSWORD_RESET','489e781f-9134-4c76-985e-b4d39781cc87', now(), 1);
-insert into IMDBCLONE.verification_token  (verification_type,token,expiry_date_in_utc,account_id) values('EMAIL_CONFIRMATION','0d971513-ea59-4ab7-87c6-8ba032937a9c', now(), 1);
-
 # mandatory inserts
 insert into IMDBCLONE.role (name) values('ROLE_ADMIN');
 insert into IMDBCLONE.role (name) values('ROLE_USER');
+
+
+# test data
+insert into IMDBCLONE.account(id,username,email,password,first_name,last_name,bio,phone,birthday,locked,enabled) values(1,'les_grossman','tom.cruise@yahoo.de','UnencryptedPa55worD','Tom','Cruise','I will massacre you!','01628264723', '1962-07-03 00:00:00',false,true);
+insert into IMDBCLONE.account(id,username,email,password,first_name,last_name,bio,phone,birthday,locked,enabled) values(2,'jeff_portnoy','jack.black@gmail.com','UnencryptedPa55worD','jack','black','You go, girl!','015122973088', '1969-08-28 00:00:00',false,true);
+
+insert into IMDBCLONE.rating(rating,movie_id,account_id) values(9.2,942385,1);
+insert into IMDBCLONE.rating(rating,movie_id,account_id) values(7.7,369339,1);
+insert into IMDBCLONE.rating(rating,movie_id,account_id) values(7.8,942385,2);
+insert into IMDBCLONE.rating(rating,movie_id,account_id) values(8.1,332379,2);
+
+insert into IMDBCLONE.watched_movie(movie_id,account_id) values(942385,1);
+insert into IMDBCLONE.watched_movie(movie_id,account_id) values(369339,1);
+insert into IMDBCLONE.watched_movie(movie_id,account_id) values(942385,1);
+
+insert into IMDBCLONE.comment(message,movie_id,account_id) values('What an outrageous cast!',942385,1);
+insert into IMDBCLONE.comment(message,movie_id,account_id) values('The shoot was sometimes difficult.',369339,1);
+insert into IMDBCLONE.comment(message,movie_id,account_id) values('All except for one ;-)',942385,2);
+insert into IMDBCLONE.comment(message,movie_id,account_id) values('It was fun to play!',332379,2);
