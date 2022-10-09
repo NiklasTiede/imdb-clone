a348a7319da71c58881d6c8005d4b4d7c6dea1fa
Niklas Tiede <niklastiede2@gmail.com>
2022-10-09 16:34:16 +0200

    migrate away from deprecated WebSecurityConfigurerAdapter

--- src/main/java/com/thecodinglab/imdbclone/config/WebSecurityConfig.java
+++ src/main/java/com/thecodinglab/imdbclone/config/WebSecurityConfig.java
@@ -2,43 +2,37 @@ package com.thecodinglab.imdbclone.config;
 
 import com.thecodinglab.imdbclone.security.JwtAuthenticationEntryPoint;
 import com.thecodinglab.imdbclone.security.JwtAuthenticationFilter;
-import com.thecodinglab.imdbclone.service.impl.CustomUserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-  private final CustomUserDetailsServiceImpl customUserDetailsService;
   private final JwtAuthenticationEntryPoint unauthorizedHandler;
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   public WebSecurityConfig(
-      CustomUserDetailsServiceImpl customUserDetailsService,
       JwtAuthenticationEntryPoint unauthorizedHandler,
       JwtAuthenticationFilter jwtAuthenticationFilter) {
-    this.customUserDetailsService = customUserDetailsService;
     this.unauthorizedHandler = unauthorizedHandler;
     this.jwtAuthenticationFilter = jwtAuthenticationFilter;
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors()
         .and()
         .csrf()
@@ -62,18 +56,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .authenticated();
 
     http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
   }
 
-  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder)
-      throws Exception {
-    authenticationManagerBuilder
-        .userDetailsService(customUserDetailsService)
-        .passwordEncoder(passwordEncoder());
-  }
-
-  @Bean(BeanIds.AUTHENTICATION_MANAGER)
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 
   @Bean
