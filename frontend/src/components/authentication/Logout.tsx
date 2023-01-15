8c3ae1e5a000275bf06384a7bf1c5eaa213c29d0
Niklas Tiede <niklastiede2@gmail.com>
2023-01-15 20:18:42 +0100

    redirect after login to home-page

--- frontend/src/components/authentication/Logout.tsx
+++ frontend/src/components/authentication/Logout.tsx
@@ -3,12 +3,17 @@ import { tokens } from "../../theme";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { Dispatch } from "../../redux/store";
+import { useEffect } from "react";
 
 const Logout = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const navigateTo = useNavigate();
   const dispatch = useDispatch<Dispatch>();
+
+  useEffect(() => {
+    navigateTo("/home");
+  }, []);
 
   return <div>You're now logged out!</div>;
 };
