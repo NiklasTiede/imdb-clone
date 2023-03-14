a429b28a4f7a052e035bd2e56d7b5b3560331837
Niklas Tiede <niklastiede2@gmail.com>
2023-03-14 20:28:49 +0100

    add bucket

--- src/main/java/com/thecodinglab/imdbclone/CommandLineRunnerImpl.java
+++ src/main/java/com/thecodinglab/imdbclone/CommandLineRunnerImpl.java
@@ -4,11 +4,13 @@ import com.thecodinglab.imdbclone.entity.Movie;
 import com.thecodinglab.imdbclone.repository.*;
 import com.thecodinglab.imdbclone.repository.MovieElasticSearchRepository;
 import com.thecodinglab.imdbclone.service.ElasticSearchService;
+import com.thecodinglab.imdbclone.service.FileStorageService;
 import com.thecodinglab.imdbclone.utility.PartitionList;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
@@ -22,25 +24,29 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
   private final MovieRepository movieRepository;
   private final MovieElasticSearchRepository movieSearchRepository;
   private final ElasticSearchService elasticSearchService;
-
   private final ElasticsearchOperations elasticsearchOperations;
+  private final FileStorageService fileStorageService;
+
+  @Value("${minio.rest.bucketName}")
+  public String bucketName;
 
   public CommandLineRunnerImpl(
       MovieRepository movieRepository,
       MovieElasticSearchRepository movieSearchRepository,
       ElasticSearchService elasticSearchService,
-      ElasticsearchOperations elasticsearchOperations) {
+      ElasticsearchOperations elasticsearchOperations,
+      FileStorageService fileStorageService) {
     this.movieRepository = movieRepository;
     this.movieSearchRepository = movieSearchRepository;
     this.elasticSearchService = elasticSearchService;
     this.elasticsearchOperations = elasticsearchOperations;
+    this.fileStorageService = fileStorageService;
   }
 
   @Transactional
   @Override
   public void run(String... arg0) {
-
-    // indexing a set of movies if no movies can be found in ES
+    // indexing a set of movies if no movies can be found in Elasticsearch
     if (elasticsearchOperations.indexOps(IndexCoordinates.of("movies")).exists()
         && movieSearchRepository.count() < 100000) {
       List<Movie> popularMovies = movieRepository.findByImdbRatingCountBetween(100, 20000000);
@@ -48,5 +54,8 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
       List<List<Movie>> partitions = PartitionList.partition(popularMovies, 1000);
       partitions.forEach(elasticSearchService::indexMovies);
     }
+
+    // create bucket/directories and set bucket policy in minIO if not existent
+    fileStorageService.setUpBucket();
   }
 }
