7b825b6ddbeeb602d84a7d7fd99910fca3ffa54d
Tiede <niklastiede2@gmail.com>
2023-04-30 16:39:28 +0200

    make host names injectable (docker-compose container names are not resolved with 2 docker-compose files) and fix credentials generation on linux

--- infrastructure/deployment/production/generate_credentials.sh
+++ infrastructure/deployment/production/generate_credentials.sh
@@ -3,7 +3,7 @@
 # Generate a random password
 generate_password() {
   length=$1
-  openssl rand -base64 $((length * 3/4)) | tr -dc 'A-Za-z0-9!@#$%^&*()_-+=' | head -c $length
+  openssl rand -base64 $((length * 3/4)) | tr -dc 'A-Za-z0-9!@#$%^&*()+' | head -c $length
 }
 
 MYSQL_ROOT_PASSWORD_ENV_VAR=$(generate_password 16)
@@ -15,6 +15,13 @@ MINIO_SECRET_KEY_ENV_VAR=$(generate_password 16)
 
 ELASTIC_USERNAME_ENV_VAR=elastic
 ELASTIC_PASSWORD_ENV_VAR=$(generate_password 16)
+
+MYSQL_HOST_ENV_VAR=robotnik-um560
+ELASTICSEARCH_HOST_ENV_VAR=robotnik-um560
+MINIO_HOST_ENV_VAR=robotnik-um560
+
+FRONTEND_APP_ADDRESS_ENV_VAR=http://robotnik-um560:80
+BACKEND_APP_ADDRESS_ENV_VAR=http://robotnik-um560:8080
 
 MAIL_USERNAME_ENV_VAR=your-own-email@email.com
 MAIL_PASSWORD_ENV_VAR=your-own-password
@@ -35,8 +42,12 @@ ELASTIC_USERNAME_ENV_VAR=$ELASTIC_USERNAME_ENV_VAR
 ELASTIC_PASSWORD_ENV_VAR=$ELASTIC_PASSWORD_ENV_VAR
 
 # Spring Boot Backend:
-FRONTEND_APP_ADDRESS_ENV_VAR=http://robotniks-deployment-server:80
-BACKEND_APP_ADDRESS_ENV_VAR=http://robotniks-deployment-server:8080
+MYSQL_HOST_ENV_VAR=$MYSQL_HOST_ENV_VAR
+ELASTICSEARCH_HOST_ENV_VAR=$ELASTICSEARCH_HOST_ENV_VAR
+MINIO_HOST_ENV_VAR=$MINIO_HOST_ENV_VAR
+
+FRONTEND_APP_ADDRESS_ENV_VAR=$FRONTEND_APP_ADDRESS_ENV_VAR
+BACKEND_APP_ADDRESS_ENV_VAR=$BACKEND_APP_ADDRESS_ENV_VAR
 
 MAIL_USERNAME_ENV_VAR=$MAIL_USERNAME_ENV_VAR
 MAIL_PASSWORD_ENV_VAR=$MAIL_PASSWORD_ENV_VAR
