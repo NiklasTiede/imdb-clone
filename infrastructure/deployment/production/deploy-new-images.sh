9139ef323701e532a4054f483d6af58e23b43bed
Tiede <niklastiede2@gmail.com>
2023-05-01 20:56:37 +0200

    do not inject host via docker-compose instead just define in .env.production

--- infrastructure/deployment/production/deploy-new-images.sh
+++ infrastructure/deployment/production/deploy-new-images.sh
@@ -5,6 +5,7 @@ sudo sysctl -w vm.max_map_count=262144
 
 # set up for next deployment
 sudo docker-compose -f docker-compose.stateful-apps.yaml down
+sudo docker rmi niklastiede/imdb-clone-frontend niklastiede/imdb-clone-backend
 sudo docker-compose -f docker-compose.stateful-apps.yaml up -d
 
 echo "Latest image of the imdb-clone was deployed!"
