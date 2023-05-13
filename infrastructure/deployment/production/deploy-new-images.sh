09a2e7ae35ca126d38771ac45fcc154bbeb85830
Tiede <niklastiede2@gmail.com>
2023-05-13 02:42:58 +0200

    fix redeployment script

--- infrastructure/deployment/production/deploy-new-images.sh
+++ infrastructure/deployment/production/deploy-new-images.sh
@@ -4,8 +4,8 @@
 sudo sysctl -w vm.max_map_count=262144
 
 # set up for next deployment
-sudo docker-compose -f docker-compose.stateful-apps.yaml down
+sudo docker-compose -f docker-compose.stateless-apps.yaml down
 sudo docker rmi niklastiede/imdb-clone-frontend niklastiede/imdb-clone-backend
-sudo docker-compose -f docker-compose.stateful-apps.yaml up -d
+sudo docker-compose -f docker-compose.stateless-apps.yaml up -d
 
 echo "Latest image of the imdb-clone (FE/BE) was deployed!"
