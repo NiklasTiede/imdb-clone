3cab91eab35671c4fc37472578a215edbfe4a7cd
Tiede <niklastiede2@gmail.com>
2023-08-11 18:22:31 +0200

    allowed fields for logging with ELK (prevent fields number explosion)

---
+++ src/main/java/com/thecodinglab/imdbclone/utility/LogField.java
@@ -0,0 +1,20 @@
+package com.thecodinglab.imdbclone.utility;
+
+/** These values can be logged */
+public class LogField {
+  public static final String ACCOUNT_ID = "accountId";
+  public static final String ACCOUNT_IDS = "accountIds";
+
+  public static final String MOVIE_ID = "movieId";
+  public static final String MOVIE_IDS = "movieIds";
+
+  public static final String COMMENT_ID = "commentId";
+  public static final String COMMENT_IDS = "commentIds";
+
+  public static final String MOVIE_GENRE = "MovieGenreEnum";
+  public static final String MOVIE_TYPE = "MovieTypeEnum";
+  public static final String ROLE_NAME = "RoleNameEnum";
+  public static final String VERIFICATION_TYPE = "VerificationTypeEnum";
+
+  private LogField() {}
+}
