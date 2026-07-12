15729e4dbce80bb3e83c9e11b4b2ffe5cd5ef87d
Tiede <niklastiede2@gmail.com>
2026-07-12 12:43:50 +0200

    fix(modulith): declare shared contracts

--- src/main/java/com/thecodinglab/imdbclone/recommendation/package-info.java
+++ src/main/java/com/thecodinglab/imdbclone/recommendation/package-info.java
@@ -1,7 +1,12 @@
 @ApplicationModule(
     id = "recommendation",
     displayName = "Recommendations",
-    allowedDependencies = {"catalog::recommendation", "shared::error"})
+    allowedDependencies = {
+      "catalog::recommendation",
+      "shared::error",
+      "shared::persistence",
+      "shared::security"
+    })
 package com.thecodinglab.imdbclone.recommendation;
 
 import org.springframework.modulith.ApplicationModule;
